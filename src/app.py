7305c1af6bc8a01c22391c4335415aa1f1bfe0b2
Lunatico97 <lunatico123@gmail.com>
2024-01-31 12:29:35 +0545

    Progess Bar & Responsiveness

--- src/app.py
+++ src/app.py
@@ -26,7 +26,7 @@ st.header("Zap through your medical papers and articles") ;
 st_model_load = st.text('!!!!! WAIT --- Setting up environment !!!!!') ;
 
 # Initialize summarizer with model card
-summarizer = Summarizer(model_card = "DiwasDiwas/t5-small-medicoSummarizer") ;
+summarizer = Summarizer(model_card = "DiwasDiwas/t5-small-ZapMed") ;
 summarizer.kickstart_model() ;
 st.success('⚡ Are you ready to zap in an instant !? ⚡') ;
 st_model_load.text("") ;
@@ -45,30 +45,37 @@ if 'ngrams_norepeat' not in st.session_state:
 if 'beams' not in st.session_state:
     st.session_state.beams = 4 ;
 
+# On-Change Functions
+def on_change_max_length():
+        st.session_state.max_length = max_length ;
+
+def on_change_length_penalty():
+    st.session_state.length_penalty = length_penalty ;
+
+def on_change_temperature():
+   st.session_state.temperature = temperature ;
+
+def on_change_repitition_penalty():
+    st.session_state.repitition_penalty = repitition_penalty ;
+
+def on_change_beams():
+    st.session_state.beams = beams ;
+
+def on_change_ngrams_norepeat():
+    st.session_state.ngrams_norepeat = ngrams_norepeat ;
+
 # Sidebar components with sliders to configure model parameters
 with st.sidebar:
     st.image('images/zapmed.png') ;
     st.header("Options") ;
-    def on_change_max_length():
-        st.session_state.max_length = max_length ;
     max_length = st.slider("Generation Length", min_value=50, max_value=128, value=100, step=5, on_change=on_change_max_length) ;
-    def on_change_length_penalty():
-        st.session_state.length_penalty = length_penalty ;
     length_penalty = st.slider("Length Penalty", min_value=1, max_value=10, value=2, step=1, on_change=on_change_length_penalty) ;
     advanced = st.sidebar.checkbox(label="Advanced Options ") ;
     if advanced:  
-        def on_change_temperature():
-            st.session_state.temperature = temperature ;
         temperature = st.slider("Temperature", min_value=0.1, max_value=1.5, value=0.8, step=0.05, on_change=on_change_temperature) ;
-        st.markdown("_[Diversity / Randomness]_")
-        def on_change_repitition_penalty():
-            st.session_state.length_penalty = repitition_penalty ;
-        repitition_penalty = st.slider("Repitition Penalty", min_value=1.0, max_value=10.0, value=5.0, step=1.0, on_change=on_change_repitition_penalty) ;
-        def on_change_beams():
-            st.session_state.beams = beams ;
+        st.markdown("_[Diversity / Randomness]_")        
+        repitition_penalty = st.slider("Repitition Penalty", min_value=1.0, max_value=10.0, value=5.0, step=1.0, on_change=on_change_repitition_penalty) ;        
         beams = st.slider("Beam searches", min_value=1, max_value=10, value=4, step=1, on_change=on_change_beams) ;
-        def on_change_ngrams_norepeat():
-            st.session_state.ngrams_norepeat = ngrams_norepeat ;
         ngrams_norepeat = st.slider("'N' words No-repeat", min_value=1, max_value=5, value=2, step=1, on_change=on_change_ngrams_norepeat) ;
     else:
         temperature = 0.8 ;
@@ -128,13 +135,16 @@ with tab2:
         summaries = [] ;
         with tab3:
             text = PDF2Text(filename) ;
+            print(text) ;
             with st.container(border=True):
                 st.subheader("Extracted Text:- ") ;
                 st.markdown(text[0]) ;
 
         with tab4:
+            progress_text = "Operation in progress. Please wait !!!" ;
+            loadBar = st.progress(0, text=progress_text) ;
             chunks = textToChunks(text[0]) ;
-            for chunk in chunks:
+            for index, chunk in enumerate(chunks):
                 chunk = cleanText(chunk) ;
                 summaries.append(
                     summarizer.generate_summary(
@@ -146,14 +156,19 @@ with tab2:
                         beams = beams
                     )[0] 
                 ) ;
+                loadBar.progress((index+1)/len(chunks), text=progress_text + f'{((index+1)/len(chunks))*100} %') ;
+            st.markdown("Operation successfully completed !") ;
             finalSum = ' '.join(summaries) ;
-            with open(f"{base_path}/uploads/summary_{name}.txt" , "w") as f:
+            with open(f"{base_path}/output/summary_{name}.txt" , "w") as f:
                 f.write(finalSum) ;
             print('Summary stored at /output directory !!') ;
             f.close() ;
             with st.container(border=True):
+                with open(f"{base_path}/output/summary_{name}.txt", "r") as f:
+                    output = f.readlines() ;
+                f.close() ;
                 st.subheader("Summarized Text:- ") ;
-                st.markdown(finalSum) ;
+                st.markdown(output) ;
 
     else:
         st.info("Please upload a PDF file ! ") ;
